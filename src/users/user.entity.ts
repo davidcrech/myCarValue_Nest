052460e492d2f6a105c162ffa527c3da40c9c0ee
davidcrech <davidcrech220@gmail.com>
2025-06-17 14:05:58 -0300

    feat: add unit testing to auth.service

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,5 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
-import { Report } from 'src/reports/report.entity';
+import { Report } from '../reports/report.entity';
 
 @Entity()
 export class User {
